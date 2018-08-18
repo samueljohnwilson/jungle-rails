e2590ecef73555578e22fbc0590c0d5f2ca3e656
samueljohnwilson <samjwilson@gmail.com>
2018-08-18 18:19:41 +0000

    Update review display on products#index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @reviews = Review.all.order(created_at: :desc)
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
   has_many :reviews
 
   has_secure_password
