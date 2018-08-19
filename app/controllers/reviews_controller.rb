fcd58d0edbc2e49fa9f49983b4838b15a8f210af
samueljohnwilson <samjwilson@gmail.com>
2018-08-19 15:45:07 +0000

    Repair all bugs

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < ApplicationController
+
+  #before_action :authenticate_user, only: [:create, :destroy]
+
   def create
     @review = Review.new(review_params)
     if @review.save
